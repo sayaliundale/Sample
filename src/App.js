e8e4dd2ab0ac32c23708f8a8ca94e242c1cd5063
sayali <sayaliundale@gmail.com>
2024-01-09 14:28:40 +0530

    new

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import React from 'react';
 import Header from './Header';
 
-const App = () =>{
+const App = () => {
   return (
     <>
-    <Header/>
+      <Header />
       <h2>Hello Sayali!</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quasi accusamus in sint
+       velit tempore illo vitae soluta itaque blanditiis.</p>
     </>
   )
 }
