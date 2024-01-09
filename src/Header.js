3c3660931214893ac0b665d52398c9894c0d40d1
sayali <sayaliundale@gmail.com>
2024-01-09 14:20:52 +0530

    Chnages are made

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='main'>
+        <h2 style={{color:"red"}}>Welcome to React World!</h2>
+    </div>
+  )
+}
+
+export default Header
